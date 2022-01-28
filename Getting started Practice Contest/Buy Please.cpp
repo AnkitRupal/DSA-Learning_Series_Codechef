0c0c8f4da322228c1d2b2824395e8df2c257436a
Ankit Rupal <Ankit301001@gmail.com>
2022-01-28 23:42:48 +0530

    Intial Commit

---
+++ Getting started Practice Contest/Buy Please.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+	BUY PLEASE
+	Total Price = (Number of Pens * Cost of one Pen) + (Number of Pencils * Cost of one Pencil)
+*/
+
+int main() {
+	int a, b, x, y;
+	cin >> a >> b >> x >> y;
+	cout << (a * x) + (b * y) << "\n";
+	return 0;
+}
