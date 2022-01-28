90b71e32595da5d3edf2fad68b11989509729a12
Ankit Rupal <Ankit301001@gmail.com>
2022-01-29 02:16:13 +0530

    Created Is_Both_Or_Not.cpp

---
+++ Getting started Practice Contest/Is_Both_Or_Not.cpp
@@ -0,0 +1,20 @@
+/*
+    IS BOTH OR NOT 
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%5==0){
+        if(n%11==0){
+            cout << "BOTH\n";
+        }else{
+            cout << "ONE\n";
+        }
+    }else{
+        cout << "NONE\n";
+    }
+    return 0;
+}
