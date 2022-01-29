5d5ef7cdec7027c1a7af902cc9146943fb566da3
Ankit Rupal <Ankit301001@gmail.com>
2022-01-29 14:40:04 +0530

    Created Raju_And_His_Trip.cpp

---
+++ Getting started Practice Contest/Raju_And_His_Trip.cpp
@@ -0,0 +1,20 @@
+/*
+    RAJU AND HIS TRIP
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 5 == 0 || n % 6 == 0)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
