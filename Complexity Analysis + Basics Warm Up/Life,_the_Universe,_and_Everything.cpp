abbda700ec2d7ab52ca4a098a753447896df74d7
Ankit Rupal <Ankit301001@gmail.com>
2022-01-29 15:59:48 +0530

    Started Complexity Analysis + Basics Warm Up
    Created a Folder named Complexity Analysis + Basics Warm Up and created a File named Life,_the_Universe,_and_Everything.cpp

---
+++ Complexity Analysis + Basics Warm Up/Life,_the_Universe,_and_Everything.cpp
@@ -0,0 +1,16 @@
+/*
+    LIFE, THE UNIVERSE, AND EVERYTHING
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int x;
+    while (cin >> x){
+        if(x==42){
+            break;
+        }
+        cout << x << "\n";
+    }
+    return 0;
+}
