ffcd1bdae14f3a773df70acc7cd5bd274399cb97
Ankit Rupal <Ankit301001@gmail.com>
2022-01-29 15:09:17 +0530

    Creadted Reverse_Star_Pattern.cpp

---
+++ Getting started Practice Contest/Reverse_Star_Pattern.cpp
@@ -0,0 +1,24 @@
+/*
+    REVERE STAR PATTERN
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int row_count = 1; row_count <= n; row_count++)
+    {
+        for (int spaces = 1; spaces <= n - row_count; spaces++)
+        {
+            cout << " ";
+        }
+        for (int stars = 1; stars <= row_count; stars++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
