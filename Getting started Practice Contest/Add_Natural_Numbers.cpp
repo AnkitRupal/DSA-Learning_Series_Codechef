3943d7fc9edb15e4685770f1f57c2ec877da7dca
Ankit Rupal <Ankit301001@gmail.com>
2022-01-29 15:13:28 +0530

    Created Add_Natural_Numbers.cpp

---
+++ Getting started Practice Contest/Add_Natural_Numbers.cpp
@@ -0,0 +1,12 @@
+/*
+    ADD NATURAL NUMBERS
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    long long int n;
+    cin >> n;
+    cout << (n) * (n + 1) / 2<<"\n";
+    return 0;
+}
